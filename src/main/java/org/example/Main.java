5a5825d7aa8ca64686029a752b69d4dfb655360f
szrnkucuk <153090583+szrnkucuk@users.noreply.github.com>
2025-04-24 12:46:29 +0300

    completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+
+        Person person1 = new Person("John", "Doe", 20);
+        Person person2 = new Person("Ali", "Kucuk", 21,532,"xx@hotmail.com",32);
+        System.out.println("Firstname: " + person1.getFirstName());
+
+        System.out.println("LastName: " + person1.getLastName());
+
+        System.out.println("PhoneNumber: " + person2.getPhoneNumber());
+
+        System.out.println("*****SECOND PART******");
+
+        Wall wall = new Wall(5,4);
+        System.out.println("area= " + wall.getArea());
+
+        wall.setHeight(-1.5);
+
+        System.out.println("width= " + wall.getWidth());
+
+        System.out.println("height= " + wall.getHeight());
+
+        System.out.println("area= " + wall.getArea());
     }
 }
